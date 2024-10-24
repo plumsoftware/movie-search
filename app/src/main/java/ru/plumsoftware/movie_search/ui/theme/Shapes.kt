927d336e40defe7f0b65a9257277927fc811300a
Slava <plumsoftwareofficial@gmail.com>
2024-10-25 00:08:29 +0600

    app [+] Добавил тему.

---
+++ app/src/main/java/ru/plumsoftware/movie_search/ui/theme/Shapes.kt
@@ -0,0 +1,9 @@
+package ru.plumsoftware.movie_search.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    medium = RoundedCornerShape(size = 14.dp)
+)
